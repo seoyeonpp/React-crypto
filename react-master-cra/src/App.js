f300312206578a87c7a672565fde2553ae7af751
박서연 <sypark@reflexion.co.kr>
2022-12-07 14:15:10 +0900

    styled-components attrs 설정법

--- react-master-cra/src/App.js
+++ react-master-cra/src/App.js
@@ -2,25 +2,30 @@ import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
+  flex-direction: column;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-// Box의 속성을 그대로 가져옴
-const Circle = styled(Box)`
-  border-radius:50%;
+const Input = styled.input.attrs({required:true, minLength: 10})`
+  background-color: pink;
 `;
 
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal"/>
-      <Circle bgColor="tomato"/>
+    <Father as="header">
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">Log in</Btn>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
