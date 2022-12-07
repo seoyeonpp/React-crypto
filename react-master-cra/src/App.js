834ec13603309b9bc7a2079211f1cf0eb7b6da84
박서연 <sypark@reflexion.co.kr>
2022-12-07 13:55:25 +0900

    styled-components 확장 학습

--- react-master-cra/src/App.js
+++ react-master-cra/src/App.js
@@ -4,28 +4,23 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.span`
-  color: white;
+// Box의 속성을 그대로 가져옴
+const Circle = styled(Box)`
+  border-radius:50%;
 `;
+
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
     </Father>
   );
 }
