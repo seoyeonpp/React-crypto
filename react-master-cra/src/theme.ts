f6c747a7e8ca0937fb6397939bbe6f8d2d9a16c8
박서연 <seoyeonpub@gmail.com>
2022-12-12 23:13:06 +0900

    style추가

--- react-master-cra/src/theme.ts
+++ react-master-cra/src/theme.ts
@@ -1,7 +1,7 @@
 import {DefaultTheme} from "styled-components";
 
 export const theme:DefaultTheme = {
-    bgColor: "white",
-    textColor: "black",
-    btnColor: "tomato"
+    bgColor: "#F8EFBA",
+    textColor: "#2C3A47",
+    accentColor: "#FD7272",
 }
