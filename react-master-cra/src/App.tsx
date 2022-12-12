41e9555edad459a114bcfc423c48fa6ab0669c76
박서연 <seoyeonpub@gmail.com>
2022-12-12 22:12:32 +0900

    타입스크립트 복습

--- react-master-cra/src/App.tsx
+++ react-master-cra/src/App.tsx
@@ -1,20 +1,31 @@
 // import styled, { keyframes } from "styled-components";
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
   background-color: ${(props) => props.theme.bgColor};
 `;
 
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+// const H1 = styled.h1`
+//   color: ${(props) => props.theme.textColor};
+// `;
+interface DummyProps {
+  text: string;
+  active? : boolean;
+}
+
+function Dummy ({text , active=false} : DummyProps) {
+  return <h1>{text}</h1>
+};
 
 function App() {
-
+  const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+    console.log('click');
+  };
   return (
     <Container>
-      <H1>HI~~~~</H1>
+      <Dummy active text={'Hi'}/>
+      <button onClick={onClick}>click me</button>
     </Container>
   );
 }
