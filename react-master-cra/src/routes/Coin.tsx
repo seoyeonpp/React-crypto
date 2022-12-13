6ae564ce72ae496d6c29d30400c5154705b9111d
박서연 <sypark@reflexion.co.kr>
2022-12-13 15:14:16 +0900

    nested 라우터 학습

--- react-master-cra/src/routes/Coin.tsx
+++ react-master-cra/src/routes/Coin.tsx
@@ -1,6 +1,8 @@
-import { useLocation, useParams } from 'react-router-dom';
-import React, { useState,useEffect } from 'react';
+import { Switch,Route, useLocation, useParams } from 'react-router-dom';
+import React, {useState,useEffect } from 'react';
 import styled from 'styled-components';
+import Price from './Price';
+import Chart from './Chart';
 
 
 const Container = styled.div`
@@ -9,6 +11,8 @@ const Container = styled.div`
     margin: 0 auto;
 `;
 const Title = styled.h1`
+    font-weight: 700;
+    font-size: 36px;
     color : ${(props) => props.theme.accentColor};
 `;
 const Header = styled.header`
@@ -20,6 +24,27 @@ const Header = styled.header`
 const Loader = styled.span`
     display: block;
     text-align: center; 
+`;
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color:rgba(255,255,255,0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    span:first-child {
+        font-size: 10px;
+        font-weight: 700;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+const Description = styled.p`
+    margin: 20px 0px;
 `;
 
 
@@ -96,23 +121,61 @@ function Coin() {
             const infoData = await (
                 await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
             ).json();
-            console.log(infoData)
             const priceData = await (
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
-            console.log(priceData)
+
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoaing(false);
         })();
-    }, [])
+    }, [coinId])
     return (
         <Container>
             <Header>
-                <Title>{state?.name || "Loading"}</Title>
+            <Title>
+                {state?.name ? state.name : loading ? "Loading..." : info?.name}
+            </Title>
             </Header>
-            {loading ? 
-                <Loader>Loading...</Loader> : null
-            }
+            {loading ? (
+            <Loader>Loading...</Loader>
+            ) : (
+            <>
+                <Overview>
+                    <OverviewItem>
+                        <span>Rank:</span>
+                        <span>{info?.rank}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Symbol:</span>
+                        <span>${info?.symbol}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Open Source:</span>
+                        <span>{info?.open_source ? "Yes" : "No"}</span>
+                    </OverviewItem>
+                </Overview>
+                <Description>{info?.description}</Description>
+                <Overview>
+                    <OverviewItem>
+                        <span>Total Suply:</span>
+                        <span>{priceInfo?.total_supply}</span>
+                    </OverviewItem>
+                    <OverviewItem>
+                        <span>Max Supply:</span>
+                        <span>{priceInfo?.max_supply}</span>
+                    </OverviewItem>
+                </Overview>
+                <Switch>
+                    <Route path={`/${coinId}/price`}>
+                        <Price />
+                    </Route>
+                    <Route path={`/${coinId}/chart`}>
+                        <Chart />
+                    </Route>
+                </Switch>
+            </>
+        )}
         </Container>
     )
 }
