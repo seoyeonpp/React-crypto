d30d1cc7037494335751bb57491de1b8436a9185
박서연 <seoyeonpub@gmail.com>
2022-12-15 14:00:33 +0900

    Chart api로 데이터 넘기는거 학습

--- react-master-cra/src/api.ts
+++ react-master-cra/src/api.ts
@@ -10,3 +10,9 @@ export function fetchCoinInfo (coinId:string) {
 export function fetchCoinTickers (coinId:string) {
     return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) => response.json())
 }
+
+export function fetchCoinHistory ( coinId:string) {
+    // const endDate = Math.floor(Date.now() / 1000);
+    // const startDate = endDate - 60 * 60 * 24; // 일주일 전 (초,분,24시간, 7일)
+    return fetch(`https://ohlcv-api.nomadcoders.workers.dev/?coinId=${coinId}`).then((response) => response.json()) 
+}
