82d34c1b013ee6a27d1b4e7a0b06b24b3443cbb4
박서연 <seoyeonpub@gmail.com>
2022-12-15 18:47:36 +0900

    커밋

--- react-master-cra/src/routes/Chart.tsx
+++ react-master-cra/src/routes/Chart.tsx
@@ -1,14 +1,31 @@
+import ApexCharts from 'apexcharts';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import { fetchCoinHistory } from '../api';
+import ApexChart from 'react-apexcharts';
 
 interface ChartProps {
     coinId : string;
 }
+interface IHistorical {
+    time_open: number;
+    time_close: number;
+    open: string;
+    high: string;
+    low: string;
+    close: string;
+    volume: string;
+    market_cap: number;
+}
 
 function Chart({ coinId }:ChartProps) {
-    const {isLoading, data} = useQuery(['ohlcv',coinId], () => fetchCoinHistory(coinId));
-    return <h1>Chart</h1>
+    const {isLoading, data} = useQuery<IHistorical[]>(['ohlcv',coinId], () => fetchCoinHistory(coinId));
+    return <div>{isLoading ? ("Loading chart...") : (<ApexChart type='line' options={{
+        chart: {
+            width: 500,
+            height: 500,
+        }
+    }}/>)}</div>
 }
 
 export default Chart;
